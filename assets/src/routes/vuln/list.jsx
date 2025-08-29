ff657c3c6f148d1db2a380af6730201f9e556f81
fan-tastic-z <fantastic.fun.zf@gmail.com>
2025-08-29 16:25:38 +0800

    feat: web vuln list update date time format

--- assets/src/routes/vuln/list.jsx
+++ assets/src/routes/vuln/list.jsx
@@ -248,7 +248,13 @@ const handleSourceFilterChange = (e) => {
                             <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd" />
                           </svg>
                           <p>
-                            更新时间: <time dateTime={vuln.updated_at}>{new Date(vuln.updated_at).toLocaleDateString('zh-CN')}</time>
+                            更新时间: <time dateTime={vuln.updated_at}>{new Date(vuln.updated_at).toLocaleString('zh-CN', {
+                              year: 'numeric',
+                              month: '2-digit',
+                              day: '2-digit',
+                              hour: '2-digit',
+                              minute: '2-digit'
+                            })}</time>
                           </p>
                           <span className={`ml-4 inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium ${
                             vuln.pushed ? 'bg-green-100 text-green-800' : 'bg-gray-100 text-gray-800'
